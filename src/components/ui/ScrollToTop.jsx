0052a4bacbf61b46a3df2b70a627792a0d690a62
fukuda <osaka481859@gmail.com>
2025-07-15 21:34:44 +0900

    GA4機能を導入

--- src/components/ui/ScrollToTop.jsx
+++ src/components/ui/ScrollToTop.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    ReactGA.send({ hitType: "pageview", page: pathname });
   }, [pathname]);
 
   return null;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-9052XM444S');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
